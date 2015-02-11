fadbb2bd073075ed49c6c385de188899185347e5
Lucky Thunder <lucasthund3r@gmail.com>
2015-02-11 00:54:08 -0200

    Tratando evento SDL_QUIT

--- main.cpp
+++ main.cpp
@@ -6,56 +6,70 @@
 
 using namespace std;
 
-void foobar(SDL_Window *window)
-{
-    /* Clear our buffer with a red background */
-    glClearColor ( 1.0, 0.0, 0.0, 1.0 );
-    glClear ( GL_COLOR_BUFFER_BIT );
-    /* Swap our back buffer to the front */
-    SDL_GL_SwapWindow(window);
-    /* Wait 2 seconds */
-    SDL_Delay(2000);
- 
-    /* Same as above, but green */
-    glClearColor ( 0.0, 1.0, 0.0, 1.0 );
-    glClear ( GL_COLOR_BUFFER_BIT );
-    SDL_GL_SwapWindow(window);
-    SDL_Delay(2000);
- 
-    /* Same as above, but blue */
-    glClearColor ( 0.0, 0.0, 1.0, 1.0 );
-    glClear ( GL_COLOR_BUFFER_BIT );
-    SDL_GL_SwapWindow(window);
-    SDL_Delay(2000);
-}
- 
 /* Our program's entry point */
 int main(int argc, char *argv[])
 {
+    SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *mainwindow; /* Our window handle */
     SDL_GLContext maincontext; /* Our opengl context handle */
- 
+
     /* Turn on double buffering with a 24bit Z buffer.
      * You may need to change this to 16 or 32 for your system */
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
- 
+
     /* Create our window centered at 512x512 resolution */
     mainwindow = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_CENTERED,
                                   SDL_WINDOWPOS_CENTERED,
-                                  512, 512, SDL_WINDOW_OPENGL);
- 
+                                  512, 512, SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
+
+    SDL_Renderer* render = SDL_CreateRenderer(mainwindow, -1,
+                           SDL_RENDERER_ACCELERATED| SDL_RENDERER_PRESENTVSYNC);
+
     /* Create our opengl context and attach it to our window */
     maincontext = SDL_GL_CreateContext(mainwindow);
 
     cout << reinterpret_cast<const char*>(glGetString(GL_VERSION)) << endl;
-  
+
     /* This makes our buffer swap syncronized with the monitor's vertical refresh */
     SDL_GL_SetSwapInterval(1);
- 
-    foobar(mainwindow);
 
-    /* Delete our opengl context, destroy our window, and shutdown SDL */
+    bool exitp = false;
+    SDL_Event event;
+
+    SDL_SetRenderDrawColor(render, 250, 250, 250, 250);
+
+    int fpsCount = 0;
+
+    GLfloat colorVector[3][4] = {{1.0, 0.0, 0.0, 1.0 },
+                                 {0.0, 1.0, 0.0, 1.0 },
+                                 {0.0, 0.0, 1.0, 1.0}};
+    unsigned posColor = 0;
+
+    while(!exitp)
+    {
+      fpsCount++;
+      SDL_PollEvent(&event);
+
+      if(event.type == SDL_QUIT)
+        exitp= true;
+
+      SDL_RenderClear(render);
+
+      if(fpsCount%60 == 0)
+        ++posColor;
+
+      glClearColor(colorVector[posColor%3][0], colorVector[posColor%3][1],
+                   colorVector[posColor%3][2], colorVector[posColor%3][3]);
+
+      glClear (GL_COLOR_BUFFER_BIT);
+
+      SDL_RenderPresent(render);
+
+      if(fpsCount == 60)
+        fpsCount = 0;
+     }
+
     SDL_GL_DeleteContext(maincontext);
     SDL_DestroyWindow(mainwindow);
     SDL_Quit();
